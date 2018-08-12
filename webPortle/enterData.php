b6dc52083984461745a2a3cdc0bf2348dfb95fad
MacSantaCruz <santacrm@oregonstate.edu>
2018-08-11 18:27:04 -0700

    Need for other comp

---
+++ webPortle/enterData.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+		$currentpage="enterData";
+		include "pages.php";
+		
+?>
+<html>
+	<head>
+		<title>Submit Data</title>
+		<link rel="stylesheet" href="index.css">
+		<script type = "text/javascript"  src = "verifyInput.js" > </script> 
+	</head>
+
+<body>
+
+
+<?php
+    include "header.php";
+    include 'connectvars.php'; 
+?>
