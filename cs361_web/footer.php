ae0b1848df6e6453aaa89100e053492379943f85
thePengwinn <38027160+thePengwinn@users.noreply.github.com>
2018-08-17 20:55:09 -0800

    print page
    added "print this page" to footer

--- cs361_web/footer.php
+++ cs361_web/footer.php
@@ -1,3 +1,6 @@
 <div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit"><a href= 'settings.php'> change settings</a></p></div>
 <div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit"><a href= 'error.php'> force an error (for testing)</a></p></div>
+<div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit"><a href="#" onClick=window.print()>print this page</a></p></div>
+
+
 <div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit">Mood App &copy 2018, CS361 Group 12</p></div>
