2cf9e82556e9e269c3fe33250d033792a70de0a2
thePengwinn <38027160+thePengwinn@users.noreply.github.com>
2018-08-17 20:44:01 -0800

    added button to force error to show customer error reporting

--- cs361_web/footer.php
+++ cs361_web/footer.php
@@ -1,2 +1,3 @@
 <div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit"><a href= 'settings.php'> change settings</a></p></div>
+<div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit"><a href= 'error.php'> force an error (for testing)</a></p></div>
 <div style="text-align:right; padding: 0.5em 1em 0 0;"><p class="credit">Mood App &copy 2018, CS361 Group 12</p></div>
